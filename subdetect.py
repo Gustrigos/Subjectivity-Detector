d44dfd7e9d7bfb7e9e0f383f91ed1124da90aff2
Gustrigos <39495859+Gustrigos@users.noreply.github.com>
2018-07-23 12:37:36 -0500

    Vocabulary list
    Build a dictionary by concatenating all the articles into a corpus, tokenize every word by filtering the corpus with the engineered features, converting each article into indices and into feature vectors.

--- subdetect.py
+++ subdetect.py
@@ -56,3 +56,77 @@ def  preProcess ( article ):
     
 	return article
 
+# 1.2) Tokenization of simplified articles. (Ordered list of Strings and Indices)
+
+def article2TokenList( raw_article ):
+
+	article = preProcess( raw_article )
+
+	# Split the article into individual words (tokens) (split by many delimeters)
+	tokens = word_tokenize(article)
+
+	# loop over each word (token) and use a stemmer to shorten it,
+	# then check if the word is in the vocab_list.  
+	# If true, store what index in the vocab_list the word is
+
+	tokenlist = []
+
+	for token in tokens:
+
+		# Remove any non alphanumeric characters
+		token = re.sub('[^a-zA-Z0-9]', '', token)
+
+		# Get rid of empty tokens
+		if not len(token): continue
+
+	# Filter words with POS tagging and store them in a list.
+	allowed_word_types = ["VBD", "PRP", "PRP$", "JJR", "RBR", "JJS", "RBS"]
+	extrafeatures = ["quote", "number", "ex-mark", "quest-mark", "monetaryval"]
+	pos = nltk.pos_tag( tokens )
+	for w in pos:
+		tokenlist.append(w[0])
+
+	return tokenlist
+
+print('Word Dictionary')
+input('Press <Enter> to continue')
+print('The following regards the simplification of articles through the tokenazation of its stemmed words: *this might take a moment to load*')
+
+# 2) Vocabulary List
+# 2.1) Build the dictionary
+# Building a dictionary by using the features with most appeareances in the whole corpus. 
+
+# Concanate all articles into one to form a corpus.
+# Tokenize each word and count it. 
+total_objective_article = [' '.join(objective_articles[:]) ]
+total_subjective_article = [' '.join(subjective_articles[:])]
+total_corpus = total_objective_article[0]+ total_subjective_article[0]
+
+word_counts = Counter(article2TokenList(total_corpus))
+
+print ('The total number of unique filtered tokens is: ')
+print (len(word_counts))
+print ('The twenty most common stemmed tokens are:')
+print ([ str(x[0]) for x in word_counts.most_common(20) ])
+
+# store tokens into a enumerated matrix. 
+common_words = [ str(x[0]) for x in word_counts.most_common(20000) ]
+vocab_dict = dict((item, i) for i, item in enumerate(common_words))
+
+# 2.2) Articles into Indices 
+
+# Converting each article into a list of these indices
+def article2VocabIndices( raw_article, vocab_dict ):
+	tokenlist = article2TokenList( raw_article )
+	index_list = [vocab_dict[token] for token in tokenlist if token in vocab_dict ]
+	return index_list
+
+# 2.3) Article into feature vector
+# detects wheter the filtered features are in an article.
+def article2FeatureVector( raw_article, vocab_dict ):
+	n = len(vocab_dict)
+	result = np.zeros((n,1))
+	vocab_indices = article2VocabIndices( raw_article, vocab_dict )
+	for idx in vocab_indices:
+		result[idx] = 1
+	return result
